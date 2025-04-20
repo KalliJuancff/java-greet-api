8716e3fec15e556c5bcf00e10699251fefb5282f
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2025-04-20 12:44:22 +0200

    feat: add current datetime to controller response message

--- src/main/java/com/juancff/greet_api/GreetController.java
+++ src/main/java/com/juancff/greet_api/GreetController.java
@@ -3,12 +3,20 @@ package com.juancff.greet_api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class GreetController {
 
-    @GetMapping("/greet")
+    @GetMapping(value = "/greet", produces = "text/plain")
     public String greet() {
-        return "¡Hola mundo!";
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+
+        return "¡Hola mundo!\n" +
+                "Fecha/hora actual: " + formattedDateTime;
     }
 
 }
