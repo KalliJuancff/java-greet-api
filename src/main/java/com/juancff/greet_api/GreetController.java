af5c5c4c97665b9835225c100671a9ed8b376df7
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2025-04-20 12:58:33 +0200

    feat: add optional parameter 'lang' which allows you to view greeting message in english

--- src/main/java/com/juancff/greet_api/GreetController.java
+++ src/main/java/com/juancff/greet_api/GreetController.java
@@ -1,22 +1,29 @@
 package com.juancff.greet_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @RestController
 public class GreetController {
 
     @GetMapping(value = "/greet", produces = "text/plain")
-    public String greet() {
+    public String greet(@RequestParam(name = "lang", required = false, defaultValue = "ES") String lang) {
+        Locale locale = "EN".equalsIgnoreCase(lang) ? Locale.ENGLISH : Locale.forLanguageTag("es-ES");
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         String formattedDateTime = now.format(formatter);
 
-        return "¡Hola mundo!\n" +
-                "Fecha/hora actual: " + formattedDateTime;
+        if ("EN".equalsIgnoreCase(lang)) {
+            return "Hello world!\n" +
+                    "Current date/time: " + formattedDateTime;
+        } else {
+            return "¡Hola mundo!\n" +
+                    "Fecha/hora actual: " + formattedDateTime;
+        }
     }
-
 }
